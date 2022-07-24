a7d561fb6613342b6bb6f91f7f2440990cf87290
rabo8497 <jeon9799@gmail.com>
2022-07-24 23:42:45 +0900

    first

---
+++ index.js
@@ -0,0 +1,141 @@
+const express = require('express')
+const fs = require('fs')
+const ejs = require('ejs')
+const mysql = require('mysql')
+const path = require("path");
+const bodyParser = require('body-parser')
+//함수 모음
+var num_po = 0
+function re_popular_file(num) {
+  const spawn = require('child_process').spawn;
+  const process = spawn('python3', ['test.py', num]); //0은 날짜를 나타낸다 -> 이후에 날짜 적용
+  process.stdout.on('data', function (data) {
+      //console.log(data.toString());
+  });
+  process.stderr.on('data', function (data) {
+      //console.log(data.toString());
+  });
+}
+
+function num_popular_file() {
+  const fs = require('fs');
+  const dir = './views/popular_pictures';
+
+  fs.readdir(dir, (err, files) => {
+    num_po =  files.length;
+  });
+}
+
+//re_popular_file(1) //날자 바꾸는 명령어
+num_popular_file()
+
+//자바스크립트
+const client = mysql.createConnection({
+  user: 'root',
+  password: 'kim00714',
+  database: 'books' 
+})
+
+const app = express()
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+///
+app.use("/views", express.static(__dirname + "/views"));
+///
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views/book_search")); 
+
+
+app.listen(52273, function () {
+  console.log('Server is running at : http://127.0.0.1:52273')
+})
+
+//start
+app.get('/', (req, res) => {
+  //console.log(num_po)
+  res.render("homePage", {data:num_po});
+});
+
+
+//READ
+app.get("/book", (req, res) => {
+  const sql = "select * from bookList";
+  client.query(sql, (err, data) => {
+    //console.log(data);
+    if(err) {
+      return console.error(err);
+    }
+    res.render("book", {model: data});
+  });
+});
+
+
+//UPDATE
+app.get("/edit/:id", (req, res)=> {
+  const id = req.params.id;
+  const sql = "SELECT * FROM bookList WHERE id=?";
+  client.query(sql, [id], (err, row)=>{
+    if(err) {
+      console.error(err.message);
+    }
+    console.log(row);
+    res.render("edit", {model:row[0]});
+  });
+});
+
+app.post("/edit/:id", (req, res)=>{
+  const id = req.params.id;
+  const book = [req.body.name, req.body.author, req.body.genre, id];
+  const sql = "UPDATE bookList SET name=?, author=?, genre=? WHERE (id = ?)";
+  client.query(sql, book, err=> {
+    if(err) {
+      console.error(err.message);
+    }
+    res.redirect("/book");
+  })
+});
+
+
+//CREATE
+app.get("/create", (req, res)=>{
+  res.render("create", {model:{} });
+});
+
+app.post("/create", (req, res)=>{
+  const book = [req.body.name, req.body.author, req.body.genre];
+  const sql = "INSERT INTO bookList (name, author, genre) VALUES (?, ?, ?)";
+  client.query(sql, book, err=> {
+    if(err){
+      console.error(err.message);
+    }
+    res.redirect("/book");
+  });
+});
+
+
+//DELETE
+app.get("/delete/:id", (req, res)=>{
+  //var isDelete = confirm(`${req.body.name}\n삭제하시겠습니까?`);
+  const id = req.params.id;
+  const sql = "SELECT * FROM bookList WHERE id=?";
+  client.query(sql, id, (err, row)=>{
+    if(err) {
+      console.error(err.message);
+    }
+    res.render("delete", {model: row[0]});
+  });
+});
+
+app.post("/delete/:id", (req, res)=> {
+  const id = req.params.id;
+  const sql = "DELETE FROM bookList WHERE id=?";
+  client.query(sql, id, err =>{
+    if(err) {
+      console.error(err.message);
+    }
+    res.redirect("/book");
+  });
+});
---
+++ test.py
@@ -0,0 +1,38 @@
+
+def hello() :
+    import sys
+    import requests
+    from bs4 import BeautifulSoup
+    import os
+    i = 0
+    day = 0
+    day = sys.argv[1]
+    while 1 :
+        if os.path.isfile("views\popular_pictures\movie{}.jpg".format(i+1)) :
+            #print(1)
+            os.remove('./views/popular_pictures/movie{}.jpg'.format(i+1))
+            i += 1
+        else :
+            break
+    res = requests.get("https://laftel.net/daily")
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    first_image = soup.find(attrs={"data-index":str(day)})
+
+    images = first_image.find_all("img")
+    image_set = []
+    j = 0
+    for idx, image in enumerate(images):
+
+        image_url = image["src"]
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+    
+        with open("./views/popular_pictures/movie{}.jpg".format(idx+1), "wb") as f: 
+            f.write(image_res.content)
+        j += 1
+    return j
+if __name__=="__main__":
+    print(hello())
+
