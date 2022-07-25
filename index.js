314ff9feb2772a081a291eeb987ab059d4adae54
rabo8497 <jeon9799@gmail.com>
2022-07-25 16:53:15 +0900

    add chat

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+
 const express = require('express')
 const fs = require('fs')
 const ejs = require('ejs')
 const mysql = require('mysql')
 const path = require("path");
 const bodyParser = require('body-parser')
+
+/*
+import express from 'express'
+import fs from 'fs'
+import ejs from 'ejs'
+import mysql from 'mysql'
+import path from 'path'
+import bodyParser from 'body-parser'
+*/
 var d = new Date();
 var week = new Array(6, 0, 1, 2, 3, 4, 5);
 var day = week[d.getDay()]; 
@@ -63,11 +73,43 @@ app.use("/views", express.static(__dirname + "/views"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views/book_search")); 
+/////////////////////////////////////////////////////////////////////////
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+/*
 app.listen(52273, function () {
   console.log('Server is running at : http://127.0.0.1:52273')
 })
+*/
 
 //start
 app.get('/', (req, res) => {
@@ -157,3 +199,116 @@ app.post("/delete/:id", (req, res)=> {
     res.redirect("/book");
   });
 });
+
+
+
+
+
+
+
+
+
+
+app.get("/request", (req, res)=>{
+  res.render("chat");
+});
+
+/////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import http from "http";
+import { Server } from "socket.io";
+*/
+const http = require("http");
+const { Server } = require("socket.io");
+
+var time_now = 0;
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer, {
+  cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true,
+  },
+});
+/*
+instrument(app, {
+  auth: false, 
+});
+*/
+function publicRooms() {
+  const {
+      sockets: {
+          adapter: { sids, rooms },
+      },
+  } =wsServer;
+  const publicRooms = [];
+  rooms.forEach((_,key) => {
+      if (sids.get(key) === undefined) {
+          publicRooms.push(key);
+      }
+  });
+  return publicRooms;
+}
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName).size;
+}
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
+  socket.onAny((event) => {
+      console.log(`Socket Event:${event}`);
+  }); //소켓 이벤트를 찾는다
+  socket.on("enter_room", (roomName, done) => {
+
+      socket.join(roomName);
+      //console.log(socket.rooms);
+      done();
+      socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));//방에 있는 모두에게 보내기
+      wsServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("disconnecting", () => {
+      socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
+  });
+  socket.on("disconnect", () => {
+      wsServer.sockets.emit("room_change", publicRooms());
+  })
+  socket.on("new_message", (msg, room, done) => {
+      socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+      done();
+  });
+  socket.on("nickname", (nickname) => {
+      socket["nickname"] = nickname;
+  });
+  ////////////////////////////////////////////
+  socket.on("give_time", (time_) => {
+      console.log(time_);
+      time_now = time_;
+      socket.broadcast.emit("same_state", time_now);
+  })
+  socket.on("choose_time", () => {
+      socket.broadcast.emit("send_yuor_time");
+  })
+  socket.on("pause1", () => {
+      socket.broadcast.emit("pause2");
+  })
+  socket.on("start1", () => {
+      socket.broadcast.emit("start2");
+  })
+  ////////////////////////////////////////////
+});
+
+httpServer.listen(52273, () => console.log("http://localhost:52273")); //서버 시작
